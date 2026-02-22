d6acf632ea05da6072f8f127b981ab8a6e30ec35
Dhiraj Agrawal <scremer61@gmail.com>
2026-02-22 21:48:50 +0530

    Railway Commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 from pymongo import MongoClient
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -45,5 +46,6 @@ def handle_message(data):
     messages_collection.insert_one(message_data)
     send(data, broadcast=True)
 
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0", port=port)
